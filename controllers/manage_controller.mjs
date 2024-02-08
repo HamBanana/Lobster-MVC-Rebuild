e1ac85dacba62f465b6f4db3f8e9fd47afaa2c75
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-02-08 04:34:01 +0100

    Reboot again

---
+++ controllers/manage_controller.mjs
@@ -0,0 +1,13 @@
+import {Controller} from '../core/controller.mjs';
+import * as sub from 'child_process';
+
+export class manage_controller extends Controller{
+perm = {'users': ['330279218543984641']}
+  index(){
+
+  }
+
+  reboot(){
+  sub.exec('../utils/reboot.sh');
+  }
+}
