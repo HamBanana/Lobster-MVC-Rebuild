d18709e70087e6c520da90c3e2696c5a43b00dec
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-02-08 19:22:07 +0100

    Even better

--- controllers/manage_controller.mjs
+++ controllers/manage_controller.mjs
@@ -15,12 +15,12 @@ perm = {'users': ['330279218543984641']}
 
   reboot(){
     this.message.reply('Rebooting..').then(() => {
-      sub.exec('/home/thawasta/codespace/lobster-utils/reboot.sh');
+      sub.exec(process.env.LOBSTER_ROOT+'../lobster-utils/reboot.sh');
     });
   }
 
   restart(){
-    let ret = sub.exec('/home/thawasta/codespace/lobster-utils/start.sh');
+    let ret = sub.exec(process.env.LOBSTER_ROOT+'../lobster-utils/start.sh');
     if (ret > 0){
       this.message.reply('Start script failed.');
       return;
