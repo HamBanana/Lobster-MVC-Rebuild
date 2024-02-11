d14cbe0b174074991a6aadc67d4b0dc46fb88348
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-02-11 18:44:51 +0100

    stringify

--- controllers/test_controller.mjs
+++ controllers/test_controller.mjs
@@ -40,7 +40,7 @@ constructor(msg){
         return this.message.reply(err.message);
       }
       db.connection.query('select count from Lobster.counting WHERE id=1', (error, results, fields) => {
-        this.message.reply('Result:'+results[0]);
+        this.message.reply('Result:'+JSON.stringify(results[0]));
       });
     })
   }
