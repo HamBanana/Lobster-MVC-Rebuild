3ec76884ab0a96afb0c3370eb8b786acf2bfae85
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-02-13 12:42:52 +0100

    Cleanup for run_backup

--- controllers/manage_controller.mjs
+++ controllers/manage_controller.mjs
@@ -78,11 +78,7 @@ perm = {'users': ['330279218543984641']
   }
 
   run_backup(args){
-    let kill = (args['kill']) ? args['kill'] : args['default'][0];
     sub.exec(process.env.LOBSTER_ROOT+'/utils/run_backup.sh');
-    if (kill){
-      //process.exit();
-    }
     this.message.react('✅');
   }
 
