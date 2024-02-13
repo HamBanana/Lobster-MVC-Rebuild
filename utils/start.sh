8f40e7dd91728c706d0ae31123b62b8f09ae39e2
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-02-13 14:36:18 +0100

    Added git commit -a -m "Commit from Pi" to start.sh

--- utils/start.sh
+++ utils/start.sh
@@ -2,6 +2,7 @@
 #LOBSTER_ROOT=/home/thawasta/codespace/Lobster-MVC-Rebuild
 cd $LOBSTER_ROOT
 git config --global --add safe.directory $LOBSTER_ROOT
+git commit -a -m "Commit from Pi"
 git pull
 npm install
 node ./index.js
