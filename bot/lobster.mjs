14dc72bf1fdfaad609c34820262ad9ff8a95370f
HamBanana <102021781+HamBanana@users.noreply.github.com>
2024-04-01 15:11:29 +0200

    Test

--- bot/lobster.mjs
+++ bot/lobster.mjs
@@ -144,7 +144,6 @@ export class Lobster{
             });
        // })
     });
-
 client.on('presenceUpdate', (oldPresence, newPresence) => {
   return;
   //let c = client.channels.cache.get('1200927450536890429');
